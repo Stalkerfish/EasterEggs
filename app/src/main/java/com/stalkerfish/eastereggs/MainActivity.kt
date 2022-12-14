82e59d5337f1075d6060676162b4b2a76bb8b4ef
Allison <silva.allison2002@gmail.com>
2022-12-14 06:27:43 -0300

    Add functionality to the Brew button: this is the first milestone for the code, counting feature has been started although isn't complete

--- app/src/main/java/com/stalkerfish/eastereggs/MainActivity.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/MainActivity.kt
@@ -2,10 +2,29 @@ package com.stalkerfish.eastereggs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var introText: TextView? = null
+    private var brewButton: Button? = null
+    private var counter: TextView? = null
+
+    var brewCounter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        introText = findViewById(R.id.intro)
+        brewButton = findViewById(R.id.brew_button)
+        counter = findViewById(R.id.counter)
+
+        counter?.text = "0"
+
+        brewButton?.setOnClickListener{
+            brewCounter++
+            counter?.text = brewCounter.toString()
+        }
     }
 }
