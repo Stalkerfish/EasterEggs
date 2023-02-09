0fb604e2fa222b71b278d522bf40ec8b25fa5e38
Allison Silva <silva.allison2002@gmail.com>
2023-02-09 07:37:40 -0300

    feat: add background music in development

--- app/src/main/java/com/stalkerfish/eastereggs/Splash.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/Splash.kt
@@ -1,23 +1,32 @@
 package com.stalkerfish.eastereggs
 
+import android.app.ActivityManager
+import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Splash : AppCompatActivity() {
     var githubIcon: ImageView? = null
     var twitterIcon: ImageView? = null
     var instagramIcon: ImageView? = null
 
-    val myGithub: String = "https://github.com/Stalkerfish/EasterEggs"
-    val myTwitter: String = "https://twitter.com/StalkerFish2"
-    val myInstagram: String = "https://www.instagram.com/stalkerfish_3/"
+    private val myGithub: String = "https://github.com/Stalkerfish/EasterEggs"
+    private val myTwitter: String = "https://twitter.com/StalkerFish2"
+    private val myInstagram: String = "https://www.instagram.com/stalkerfish_3/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        if (!isAppInforegrounded()){
+            TODO()
+        }
+
 
         githubIcon = findViewById(R.id.github)
         twitterIcon = findViewById(R.id.twitter)
@@ -40,5 +49,18 @@ class Splash : AppCompatActivity() {
             openURL.data = Uri.parse(myInstagram)
             startActivity(openURL)
         }
+    }
+
+    fun isAppInforegrounded() : Boolean {
+        val appProcessInfo =  ActivityManager.RunningAppProcessInfo();
+        ActivityManager.getMyMemoryState(appProcessInfo);
+        return (appProcessInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND ||
+                appProcessInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_VISIBLE)
+    }
+
+    override fun onDestroy() {
+        //stop service and stop music
+        stopService(Intent(this, SoundService::class.java))
+        super.onDestroy()
     }
 }
