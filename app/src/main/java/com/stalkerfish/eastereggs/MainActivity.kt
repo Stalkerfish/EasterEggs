809e2672c4e51df644fdf687e4b3a393d103ec84
Allison Silva <silva.allison2002@gmail.com>
2023-02-13 12:47:42 -0300

    feat: implements the first portal

--- app/src/main/java/com/stalkerfish/eastereggs/MainActivity.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.stalkerfish.eastereggs
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import nl.dionsegijn.steppertouch.OnStepCallback
+import nl.dionsegijn.steppertouch.StepperTouch
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
 
 class MainActivity : AppCompatActivity() {
+    var hellWardrobeHelper: WardrobeHelper? = null
+
     private var introText: TextView? = null
     private var brewButton: Button? = null
     private var counter: TextView? = null
     private var killButton: Button? = null
     private var hellGateButton: Button? = null
+    private var stepperTouch: StepperTouch? = null
 
     private var brewCounter: Int = 0
     private var killCounter: Int = 0
@@ -27,13 +35,31 @@ class MainActivity : AppCompatActivity() {
         counter = findViewById(R.id.counter)
         killButton = findViewById(R.id.kill_button)
         hellGateButton = findViewById(R.id.hellGateBtn)
+        stepperTouch = findViewById(R.id.stepperTouch)
+
+        stepperTouch?.minValue = 0
+        stepperTouch?.sideTapEnabled = true
+
+        stepperTouch?.addStepCallback(object : OnStepCallback {
+            override fun onStep(value: Int, positive: Boolean) {
+                MotionToast.createToast(this@MainActivity,
+                    null, "Oh, you raised a human!",
+                    MotionToastStyle.INFO, MotionToast.GRAVITY_BOTTOM,
+                    MotionToast.SHORT_DURATION, Typeface.SANS_SERIF )
+            }
+        })
 
         counter?.text = "0"
 
         brewButton?.setOnClickListener{
-            brewCounter++
+            /* brewCounter++
             killButton?.isVisible = true
-            counter?.text = brewCounter.toString()
+            counter?.text = brewCounter.toString() */
+            val hellOrb = HellActivity::class.java
+            hellWardrobeHelper = WardrobeHelper(this, hellOrb)
+            hellWardrobeHelper!!.onTravel()
+            hellWardrobeHelper!!.onClose(this)
+
         }
 
         hellGateButton?.setOnClickListener {
