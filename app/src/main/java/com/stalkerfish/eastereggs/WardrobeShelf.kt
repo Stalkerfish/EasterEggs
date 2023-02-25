d7a7afd3fed25dee7b5307db9245cd1f957d43ec
Allison Silva <silva.allison2002@gmail.com>
2023-02-25 04:18:45 -0300

    feat: display an empty list warning in the wardrobe dialog

--- app/src/main/java/com/stalkerfish/eastereggs/WardrobeShelf.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/WardrobeShelf.kt
@@ -1,11 +1,15 @@
 package com.stalkerfish.eastereggs
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
 
 class WardrobeShelf: DialogFragment(){
@@ -15,6 +19,8 @@ class WardrobeShelf: DialogFragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
         return inflater.inflate(R.layout.fragment_wardrobe_shelf, container, false)
     }
 
@@ -22,11 +28,15 @@ class WardrobeShelf: DialogFragment(){
         super.onViewCreated(view, savedInstanceState)
 
         val lvOrbs: ListView? = view.findViewById(R.id.lvOrbs)
+        val emptyList: TextView? = view.findViewById(R.id.emptyOrbs)
 
         val mOrbList = WardrobeHelper(requireContext()).getOrbsList()
 
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, mOrbList)
-
-        lvOrbs?.adapter = adapter
+        if (mOrbList.isEmpty())
+            emptyList?.isVisible = true
+        else {
+            val adapter = ArrayAdapter(requireContext(), R.layout.row, mOrbList)
+            lvOrbs?.adapter = adapter
+        }
     }
 }
