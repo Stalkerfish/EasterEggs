2a0e260de1b0be5dc7a620414ef3fba9023c81de
Allison Silva <silva.allison2002@gmail.com>
2023-02-27 17:15:19 -0300

    style: create a viable UI concenpt

--- app/src/main/java/com/stalkerfish/eastereggs/EarthRoom.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/EarthRoom.kt
@@ -3,13 +3,14 @@
 package com.stalkerfish.eastereggs
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class EarthRoom: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.earth_room)
-
-
     }
 }
