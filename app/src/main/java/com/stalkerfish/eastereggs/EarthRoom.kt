79705546fa5f908d07fa837dca949f565b934483
Allison Silva <silva.allison2002@gmail.com>
2023-02-27 07:08:28 -0300

    feat!: add a new room

---
+++ app/src/main/java/com/stalkerfish/eastereggs/EarthRoom.kt
@@ -0,0 +1,15 @@
+/* This Room will be everything related to The Earth Planet */
+
+package com.stalkerfish.eastereggs
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EarthRoom: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.earth_room)
+
+
+    }
+}
