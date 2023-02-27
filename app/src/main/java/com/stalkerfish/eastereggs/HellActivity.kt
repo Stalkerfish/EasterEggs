3abe732356d48bb51f29fe8e0996786d6b0365d2
Allison Silva <silva.allison2002@gmail.com>
2023-02-27 17:17:30 -0300

    refactor: add a FullScreen flag

--- app/src/main/java/com/stalkerfish/eastereggs/HellActivity.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/HellActivity.kt
@@ -2,10 +2,14 @@ package com.stalkerfish.eastereggs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class HellActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_hell)
     }
 }
