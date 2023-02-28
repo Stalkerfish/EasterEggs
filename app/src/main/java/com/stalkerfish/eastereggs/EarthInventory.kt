31340d1518da3834514d894965b3fea7cf7736ec
Allison Silva <silva.allison2002@gmail.com>
2023-02-28 08:09:37 -0300

    feat: add a dialog to Inventory Button the LinearLayout is treated as an ordinary View, so we can use OnClickListener method remove background item in EarthRoom theme, was not helping much

---
+++ app/src/main/java/com/stalkerfish/eastereggs/EarthInventory.kt
@@ -0,0 +1,54 @@
+package com.stalkerfish.eastereggs
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.fragment.app.DialogFragment
+
+class EarthInventory: DialogFragment() {
+
+    inner class EarthInventoryHelper: WardrobeHolder, AppCompatActivity(){
+        override fun onTravel(orb: Orb) {
+            TODO("Not yet implemented")
+        }
+
+        override fun Intent(context: Context): Intent {
+            TODO("Not yet implemented")
+        }
+
+        fun getList(){
+            TODO()
+        }
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        return inflater.inflate(R.layout.earth_inventory_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val itemsList: ListView = view.findViewById(R.id.inventory_items)
+        val emptySign: TextView? = view.findViewById(R.id.no_items_sign)
+
+        emptySign?.isVisible = true
+
+        // val inventoryItems = EarthInventoryHelper().getList()
+    }
+}
--- app/src/main/java/com/stalkerfish/eastereggs/EarthRoom.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/EarthRoom.kt
@@ -4,9 +4,12 @@ package com.stalkerfish.eastereggs
 
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class EarthRoom: AppCompatActivity() {
+    private var inventoryBtn: LinearLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,5 +17,12 @@ class EarthRoom: AppCompatActivity() {
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.earth_room)
+
+        inventoryBtn = findViewById(R.id.inventory)
+
+        inventoryBtn?.setOnClickListener {
+            val dialog = EarthInventory()
+            dialog.show(supportFragmentManager, dialog.tag)
+        }
     }
 }
