9eede3735939b8e20ddb248f2d36ad06b9b89973
Allison Silva <silva.allison2002@gmail.com>
2023-02-28 14:49:51 -0300

    feat: add dialogs for Inventory and TodayElement classes implements onItemClickListener in the Inventory List calls both dialogs on EarthRoom class show items on the Inventory and an empty sign if it is empty TodayElement provides a ramdom Orb for now It will be this way for a period, since what I have in mind is too advanced for me to implement with the knowledge I have right now

--- app/src/main/java/com/stalkerfish/eastereggs/EarthInventory.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/EarthInventory.kt
@@ -1,17 +1,21 @@
 package com.stalkerfish.eastereggs
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
 
 class EarthInventory: DialogFragment() {
 
@@ -19,6 +23,10 @@ class EarthInventory: DialogFragment() {
 
         fun getList(): ArrayList<Item> {
             return Inventory.getList()
+        }
+
+        fun getItemsName(): List<String?> {
+            return Inventory.getItemName()
         }
 
     }
@@ -39,12 +47,36 @@ class EarthInventory: DialogFragment() {
         val emptySign: TextView? = view.findViewById(R.id.no_items_sign)
 
         val inventoryItems = EarthInventoryHelper().getList()
+        val itemsName = EarthInventoryHelper().getItemsName()
 
         if (inventoryItems.isEmpty())
             emptySign?.isVisible = true
         else {
-            val adapter = ArrayAdapter(requireContext(), R.layout.row_earth, inventoryItems)
+            val adapter = ArrayAdapter(requireContext(), R.layout.row_earth, itemsName)
             itemsList.adapter = adapter
+
+            itemsList.onItemClickListener =
+                AdapterView.OnItemClickListener { parent, _, position, _ ->
+                    val selectedItem = parent.getItemAtPosition(position)
+
+                    val mItem = Pair(selectedItem, inventoryItems[position])
+
+                    try {
+                        mItem.second.use(requireContext())
+                        adapter.notifyDataSetChanged()
+
+                        this.dismiss()
+                    }
+                    catch (_: java.lang.IndexOutOfBoundsException) {
+                        MotionToast.createToast(
+                            requireActivity(), "Item Failed",
+                            "MAYBE IT SHOULD NOT BE USED HERE", MotionToastStyle.ERROR,
+                            MotionToast.GRAVITY_BOTTOM,
+                            MotionToast.SHORT_DURATION,
+                            Typeface.DEFAULT_BOLD
+                        )
+                    }
+                }
         }
     }
 }
--- app/src/main/java/com/stalkerfish/eastereggs/EarthRoom.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/EarthRoom.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import android.view.WindowManager
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class EarthRoom: AppCompatActivity() {
     private var inventoryBtn: LinearLayout? = null
+    private var todaysElementBtn: CardView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +21,16 @@ class EarthRoom: AppCompatActivity() {
         setContentView(R.layout.earth_room)
 
         inventoryBtn = findViewById(R.id.inventory)
+        todaysElementBtn = findViewById(R.id.todays_element)
 
         inventoryBtn?.setOnClickListener {
-            val dialog = EarthInventory()
-            dialog.show(supportFragmentManager, dialog.tag)
+            val inventoryDialog = EarthInventory()
+            inventoryDialog.show(supportFragmentManager, inventoryDialog.tag)
+        }
+
+        todaysElementBtn?.setOnClickListener {
+            val todaysDialog = TodayElement()
+            todaysDialog.show(supportFragmentManager, todaysDialog.tag)
         }
     }
 }
