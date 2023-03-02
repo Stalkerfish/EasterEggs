df8772d3fd472261d418d65fc027f454a5efeb09
Allison Silva <69297165+Stalkerfish@users.noreply.github.com>
2023-03-02 01:26:28 -0300

    style: refactor the whole UI (#12)
    * style: refactor the whole UI
    
    * refactor: remove unused drawables
    
    * style: put a little padding in the wardrobe dialog
    
    * refactor: drop Sequent library for now
    
    * refactor: remove a bug line in colors.xml file

--- app/src/main/java/com/stalkerfish/eastereggs/HomeActivity.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/HomeActivity.kt
@@ -3,19 +3,20 @@
 package com.stalkerfish.eastereggs
 
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import android.view.WindowManager
-import android.widget.Button
-import android.widget.GridLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
-import com.fujiyuu75.sequent.Sequent
 
 class HomeActivity : AppCompatActivity() {
-    private var introText: TextView? = null
-    private var wardrobe: CardView? = null
-    private var orbButton: Button? = null
-    private var grid: GridLayout? = null
+    private var wardrobe: FrameLayout? = null
+    private var backpack: FrameLayout? = null
+    private var midBtn: LinearLayout? = null
+    private var midBtnTxt: TextView? = null
+    private var journal: FrameLayout? = null
+    private var phone: FrameLayout? = null
+    private var settings: FrameLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,20 +29,20 @@ class HomeActivity : AppCompatActivity() {
 
         OrbShelf.eraseShelf()   // Erase the list of Orbs on start
 
-        introText = findViewById(R.id.intro)
         wardrobe = findViewById(R.id.wardrobe)
-        orbButton = findViewById(R.id.orb_button)
-        grid = findViewById(R.id.grid)
-
-        Sequent.origin(grid).start()    // Reveals UI items with a discrete animation
+        backpack = findViewById(R.id.bag)
+        midBtn = findViewById(R.id.mid_button)
+        midBtnTxt = findViewById(R.id.mid_button_text)
+        journal = findViewById(R.id.journal)
+        phone = findViewById(R.id.phone)
+        settings = findViewById(R.id.settings)
 
         wardrobe?.setOnClickListener {
             val dialog = WardrobeShelf()
             dialog.show(supportFragmentManager, dialog.tag)     // Display the dialog
-
         }
 
-        orbButton?.setOnClickListener { orbButtonCallback() }
+        midBtn?.setOnClickListener { orbButtonCallback() }
     }
 
     private fun orbButtonCallback() {
