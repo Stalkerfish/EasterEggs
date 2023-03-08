cd39c74a7414ccf26eaa9a040ea10d71b78bbc43
Allison Silva <69297165+Stalkerfish@users.noreply.github.com>
2023-03-08 17:53:44 -0300

    feat: Switch from DialogFragment to Fragment in some artifacts of Earth Room (#13)
    * feat: add dialogs for remaining buttons in Earth Room
    
    * Revert "Merge branch 'main' of https://github.com/Stalkerfish/EasterEggs into earth_room"
    
    This reverts commit 8bb4b44bebd1aa5bab07bfa80085a4b5a87a598f, reversing
    changes made to 2b99a880768b39be9b21f62a457cb030259cc6b1.
    
    * refactor: switch earth room dialogs to fragments filling the whole screen
    
    * chore: add a string resource
    
    * chore(build): pump gradle plugin version
    
    * chore: fix CHANGELOG history
    
    * chore(CI): update android.yml
    
    * chore(CI): rollback avd creation params

--- app/src/main/java/com/stalkerfish/eastereggs/PeriodicTable.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/PeriodicTable.kt
@@ -1,23 +1,24 @@
 package com.stalkerfish.eastereggs
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.view.isVisible
-import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 
-class PeriodicTable: DialogFragment() {
+class PeriodicTable: Fragment() {
+
+    companion object {
+        fun newInstance() = PeriodicTable()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
         return inflater.inflate(R.layout.periodic_table, container, false)
     }
 
