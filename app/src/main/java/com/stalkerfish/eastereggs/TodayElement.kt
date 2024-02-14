aff605632d6155f58297f5ad83086248edc80383
Allison Silva <69297165+Stalkerfish@users.noreply.github.com>
2024-02-14 19:18:21 -0300

    All features from Earth Room until now (#17)
    * feat: add dialogs for remaining buttons in Earth Room
    
    * Revert "Merge branch 'main' of https://github.com/Stalkerfish/EasterEggs into earth_room"
    
    This reverts commit 8bb4b44bebd1aa5bab07bfa80085a4b5a87a598f, reversing
    changes made to 2b99a880768b39be9b21f62a457cb030259cc6b1.
    
    * refactor: switch earth room dialogs to fragments filling the whole screen
    
    * chore: add a string resource
    
    * chore(build): pump gradle plugin version
    
    * chore: fix CHANGELOG history
    
    * chore(CI): update android.yml
    
    * chore(CI): rollback avd creation params
    
    * refactor: build the UI for Today's Element
    
    * feat: add hint button logic
    
    * feat: add a database with the whole periodic table
    
    * chore: add something IDE was asking(?)
    
    * feat: populate the trivia with TableOfElements items
    Earn an Orb for each element answered correctly
    
    * refactor: give an id to the root layout of home room
    
    * refactor: set Dialogs and Fragments to correspondent artifacts
    
    * feat: shows the element short-description as a free hint
    
    * refactor: adapt the texts to the screen proportions

--- app/src/main/java/com/stalkerfish/eastereggs/TodayElement.kt
+++ app/src/main/java/com/stalkerfish/eastereggs/TodayElement.kt
@@ -2,15 +2,23 @@
 
 package com.stalkerfish.eastereggs
 
+import android.app.Activity
+import kotlin.random.Random
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
 
-class TodayElement: Fragment(){
+class TodayElement: Fragment() {
 
     companion object {
         fun newInstance() = TodayElement()
@@ -27,8 +35,76 @@ class TodayElement: Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val text: TextView? = view.findViewById(R.id.not_implemented_sign)
+        val listOfElements = TableOfElements.getElement()
+        val randomElementIndex = Random.nextInt(1,listOfElements.size+1)
 
-        text?.isVisible = true
+        val atomicNumber: TextView? = view.findViewById(R.id.atomic_number)
+        val atomicWeight: TextView? = view.findViewById(R.id.atomic_weight)
+        val elementSymbol: TextView? = view.findViewById(R.id.element_symbol)
+        val hintButton: LinearLayout? = view.findViewById(R.id.hint_button)
+        val answer: EditText? = view.findViewById(R.id.answer)
+        val acceptButton: Button? = view.findViewById(R.id.accept_button)
+        val textHint: TextView? = view.findViewById(R.id.hint)
+
+        val todayElement = listOfElements[randomElementIndex]
+
+        atomicNumber?.text = todayElement.atomicNumber.toString()
+        atomicWeight?.text = todayElement.atomicWeight.toString()
+        elementSymbol?.text = todayElement.symbol
+        textHint?.text = todayElement.description
+
+        var timesClicked = 0
+
+        hintButton?.setOnClickListener {
+            timesClicked++
+
+            when (timesClicked) {
+                1 -> atomicWeight?.isVisible = true
+                2 -> atomicNumber?.isVisible = true
+                3 -> elementSymbol?.isVisible = true
+            }
+        }
+
+        acceptButton?.setOnClickListener {
+
+            if (answer?.text?.isEmpty() == true) {
+                MotionToast.darkColorToast(
+                    requireActivity() as Activity, "Missing Answer",
+                    "Input the element name in the TextBox!",
+                    MotionToastStyle.ERROR,
+                    MotionToast.GRAVITY_BOTTOM,
+                    MotionToast.SHORT_DURATION,
+                    ResourcesCompat.getFont(
+                        requireContext(),
+                        www.sanju.motiontoast.R.font.helvetica_regular
+                    )
+                )
+            }
+            else if (answer?.text contentEquals(todayElement.name)) {
+                MotionToast.darkColorToast(
+                    requireContext() as Activity, "Wow, you found out the element",
+                    "You earned a Hell Orb, it is used to travel to The Hell.",
+                    MotionToastStyle.SUCCESS,
+                    MotionToast.GRAVITY_BOTTOM,
+                    MotionToast.LONG_DURATION,
+                    ResourcesCompat.getFont(
+                        requireContext(),
+                        www.sanju.motiontoast.R.font.helvetica_regular
+                    )
+                )
+
+                val hellOrb = HellActivity::class.java
+
+                val orbAdded = Orb(hellOrb, "Hell Orb")
+                OrbShelf.addOrb(orbAdded)
+                Inventory.addItem(orbAdded)
+                close()
+            }
+        }
+    }
+
+    private fun close() {
+        val manager = requireActivity().supportFragmentManager
+        manager.beginTransaction().remove(this).commit()
     }
 }
